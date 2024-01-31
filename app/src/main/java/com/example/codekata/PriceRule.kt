7a9f5f79c36675b93c2063f358fe6980ea851e10
Sebastian Pian <Sebastian.Pian@genesisenergy.co.nz>
2024-01-31 16:24:15 +1300

    Refactor PriceRule.kt

--- app/src/main/java/com/example/codekata/PriceRule.kt
+++ app/src/main/java/com/example/codekata/PriceRule.kt
@@ -1,18 +1,16 @@
 package com.example.codekata
 
-import android.util.Log
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
-import java.io.File
 
 class PriceRule {
 
     private var priceRule = readCsv(Constants.RULE_DESTINATION)
 
-    fun readCsv(path: String): List<Rules> {
+    fun readCsv(path: String): List<Rule> {
         return csvReader().open(path) {
             readAllAsSequence().map {
                 val (sku, price, batchSize, batchPrice) = it
-                Rules(sku = sku, price = price.toFloat(), batchSize = batchSize.toIntOrNull(), batchPrice = batchPrice.toFloatOrNull())
+                Rule(sku = sku, price = price.toFloat(), batchSize = batchSize.toIntOrNull(), batchPrice = batchPrice.toFloatOrNull())
             }.toList()
 
         }
@@ -25,22 +23,23 @@ class PriceRule {
     fun calculateCartTotal(cart: LinkedHashMap<String, Int>): Float {
         var total = 0f
         cart.forEach { item ->
+            val itemAmount = item.value
             val itemRule = priceRule.first { it.sku == item.key }
             total += if (itemRule.batchSize != null && itemRule.batchPrice != null) {
-                if (item.value % itemRule.batchSize == 0) {
-                    item.value / itemRule.batchSize * itemRule.batchPrice
+                if (itemAmount % itemRule.batchSize == 0) {
+                    itemAmount / itemRule.batchSize * itemRule.batchPrice
                 } else {
-                    item.value % itemRule.batchSize * itemRule.price + item.value / itemRule.batchSize * itemRule.batchPrice
+                    itemAmount % itemRule.batchSize * itemRule.price + itemAmount / itemRule.batchSize * itemRule.batchPrice
                 }
             } else {
-                item.value * itemRule.price
+                itemAmount * itemRule.price
             }
         }
         return total
     }
 }
 
-data class Rules(
+data class Rule(
     val sku: String,
     val price: Float,
     val batchSize: Int?,
